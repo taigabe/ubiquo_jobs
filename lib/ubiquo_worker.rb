d199c61e0d938974c071232e8a1f31e667469ffa
bfcapell <bfcapell@gmail.com>
2009-04-08 12:18:17 +0200

    Added default sleep time

--- lib/ubiquo_worker.rb
+++ lib/ubiquo_worker.rb
@@ -45,7 +45,7 @@ module UbiquoWorker
   
   autoload :Worker, 'ubiquo_worker/worker'
 
-  def self.init(name, sleep_time)
+  def self.init(name, sleep_time = 5.0)
     worker = Worker.new(name, sleep_time)
     worker.run!
   end
