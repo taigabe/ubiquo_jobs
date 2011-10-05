215d997aa15d0215ea7690a9c5f6e6fedb6d6d99
Ramon Salvadó <rsalvado@gnuine.com>
2011-10-05 08:23:49 +0200

    ubiquo:worker:start task now responds to TERM signal [#591 state:resolved]

--- lib/ubiquo_worker/worker.rb
+++ lib/ubiquo_worker/worker.rb
@@ -1,18 +1,20 @@
 module UbiquoWorker
 
   class Worker
-    attr_accessor :name, :sleep_time, :shutdown
+    attr_accessor :name, :sleep_time, :sleep_interval, :shutdown
 
     def initialize(name, options = {})
       raise ArgumentError, "A worker name is required" if name.blank?
       self.name = name
       self.sleep_time = options[:sleep_time]
+      self.sleep_interval = options[:sleep_interval] || 1.0
       self.shutdown = false
     end
 
     # This method will start executing the planified jobs.
     # If no job is available, the worker will sleep for sleep_time sec.
     def run!
+      daemon_handle_signals
       while (!shutdown) do
         job = UbiquoJobs.manager.get(name)
         if job
@@ -20,12 +22,30 @@ module UbiquoWorker
           job.run!
         else
           puts "#{Time.now} [#{name}] - no job available"
-          sleep self.sleep_time
+          wait
         end
       end
     end
-        
-  end  
+
+    private
+
+    def daemon_handle_signals
+      Signal.trap("TERM") do
+        puts "Caught TERM signal, terminating ..."
+        self.shutdown = true
+      end
+    end
+
+    def wait
+      time_slept = 0
+      while time_slept < self.sleep_time
+        break if shutdown
+        sleep sleep_interval
+        time_slept = time_slept + sleep_interval
+      end
+    end
+
+  end
 end
 
 
--- test/unit/worker_test.rb
+++ test/unit/worker_test.rb
@@ -19,9 +19,19 @@ class UbiquoWorker::WorkerTest < ActiveSupport::TestCase
     start_worker :worker_options => {:sleep_time => 10.0}, :iterations => 1
   end
 
+  def test_should_set_sleep_interval
+    UbiquoWorker::Worker.any_instance.expects(:sleep_interval=).with(4.0)
+    start_worker :worker_options => {:sleep_interval => 4.0}, :iterations => 1
+  end
+
   def test_should_get_tasks
     UbiquoJobs.manager.expects(:get).with('new_worker')
     start_worker :iterations => 2
+  end
+
+  def test_should_trap_term_signal
+    Signal.expects(:trap).with("TERM")
+    start_worker :iterations => 1
   end
 
   private
@@ -35,20 +45,26 @@ class UbiquoWorker::WorkerTest < ActiveSupport::TestCase
   end
 
   def start_worker(options = {})
+    sleep_interval = 1
     options = {
       :name => 'new_worker',
       :iterations => 0,
       :worker_options => {}
     }.merge(options)
+
+    options[:worker_options][:sleep_time] ||= 5.0
+    options[:worker_options][:sleep_interval] ||= options[:worker_options][:sleep_time]
+
     shutdown_values = ([false]*(options[:iterations] > 1 ? options[:iterations]-1 : 0)) << true
 
-    UbiquoWorker::Worker.any_instance.expects(:sleep).times(0..options[:iterations]).returns(nil)
-    UbiquoWorker::Worker.any_instance.expects(:shutdown).times(options[:iterations]).returns(*shutdown_values)
+    sleep_invoked_times = options[:worker_options][:sleep_time]/options[:worker_options][:sleep_interval]
+    UbiquoWorker::Worker.any_instance.expects(:sleep).times(0..sleep_invoked_times).returns(nil)
+    UbiquoWorker::Worker.any_instance.expects(:shutdown).at_least(options[:iterations]).returns(*shutdown_values)
 
     orig_stdout = $stdout
     $stdout = File.new('/dev/null', 'w')
     UbiquoWorker.init(options[:name], options[:worker_options])
     $stdout = orig_stdout
   end
-  
+
 end
